064a237e4f2b5f7cbf48ac7b30c3b240bd2cf0a2
jaswitha.k <jaswithachowdary23@email.com>
2026-07-03 13:42:50 +0530

    second commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3002;
+
+app.use(express.static("frontend"));
+
+app.listen(PORT, function(){
+    console.log("sucessfully running at http://localhost:"+PORT);
+})
