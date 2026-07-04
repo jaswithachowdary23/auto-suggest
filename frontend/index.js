8dffefe95c0069072b35be622a979f18247b8ce6
jaswitha.k <jaswithachowdary23@email.com>
2026-07-04 10:08:15 +0530

    6th commit

--- frontend/index.js
+++ frontend/index.js
@@ -3,14 +3,15 @@ const users = [
   {
     "name" :"Jane Doe",
     "gender" : "Female",
-    "image" : "../images/jane.png"
+    "image" : "./images/jane.png.png"
   },
   {
     "name" :"John Doe",
     "gender" : "Male",
-    "image" : "../images/john.png"
+    "image" : "./images/john.png.png"
   },
 ]
+
 let curUserId = 0;
 function toggle(){
   if(curUserId == 0)
@@ -22,5 +23,29 @@ function toggle(){
   // name
   document.getElementById("card-name").innerText=users[curUserId].name;
 }
-
 console.log("Hello from JS");
+function getRandomUser()
+{
+  fetch("https://randomuser.me/api")
+    .then(function (data){
+      return data.json()
+    })
+    .then(function (parsedData){
+      // console.log(parsedData)
+      let gender = parsedData.results[0].gender;
+
+      let first = parsedData.results[0].name.first;
+      let last = parsedData.results[0].name.last;
+      let name = first  + " " + last;
+      let imageUrl = parsedData.results[0].picture.large;
+
+      // gender
+      document.getElementById("card-gender").innerText=gender;
+      // name
+      document.getElementById("card-name").innerText=name
+      // img
+      document.getElementById("img").src=imageUrl
+
+    })
+}
+
